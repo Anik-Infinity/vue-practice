7d10f93720e9cffe65d438e3e582a2f3d895aaca
Kafi <kafi@itconquest.com>
2024-10-07 23:56:36 +0600

    learn two way binding using v-model

---
+++ two-way-binding/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: ''
+        }
+    },
+    methods: {
+        // updateName(event) {
+        //     console.log('invoked')
+        //     this.name = event.target.value;
+        // },
+        resetName() {
+            this.name = ''
+        }
+    }
+})
+
+app.mount('#app')
