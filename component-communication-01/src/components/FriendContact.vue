102137a732f419effa61e993ca72c93ff097a678
Kafi <kafi@itconquest.com>
2024-11-23 19:19:01 +0600

    learn about vue-cli, component, props etc

---
+++ component-communication-01/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "phone", "email", "isFavourite"],
+  data() {
+    return {
+      detailsAreVisible: false
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
